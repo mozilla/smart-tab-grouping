68fb7ecf3bc98ef003549ab7048bf856b2df3c33
Rolf Rando <rrando@mozilla.com>
2025-02-19 20:59:26 -0800

    Fine tuning model working

--- src/jobs/tune_code.py
+++ src/jobs/tune_code.py
@@ -15,7 +15,7 @@ from transformers import (
     TrainingArguments, Trainer
 )
 
-from common.storage import upload_directory
+from src.util.storage import upload_directory
 from utils import get_bad_word_ids
 
 INPUT_PROMPT_ID = "input_with_prompt"
