f1bb4adf752ea45dc4bc7a2958a55914383067d9
Vasish Baungally <vbaungally@mozilla.com>
2025-03-24 15:55:47 -0500

    Fix bad words filter

--- src/jobs/utils.py
+++ src/jobs/utils.py
@@ -10,24 +10,18 @@ def load_words_from_url(url):
 
 def get_bad_word_ids(model_name: str = "Mozilla/smart-tab-topic"):
     tokenizer = T5Tokenizer.from_pretrained(model_name, add_prefix_space=True)
-
-    profanity = load_words_from_url(
+    bad_words_profanity = load_words_from_url(
         "https://raw.githubusercontent.com/mozilla/fx-ml-train/refs/heads/main/trust_and_safety/word_blocking/profanity.txt"
     )
-    stg_specific_words = load_words_from_url(
-        "https://raw.githubusercontent.com/mozilla/fx-ml-train/main/trust_and_safety/word_blocking/smart_tab_grouping_specific.txt"
+    bad_words_stg = load_words_from_url(
+        "https://raw.githubusercontent.com/mozilla/fx-ml-train/refs/heads/main/trust_and_safety/word_blocking/smart_tab_grouping_specific.txt"
     )
-    bad_words = list({*profanity, *stg_specific_words})
-    vocab = tokenizer.get_vocab()
-    vocab = [tokenizer.convert_tokens_to_string([token]).lower().strip() for token in vocab]
+    bad_words = list(set(list(bad_words_stg) + list(bad_words_profanity)))
+    bad_words = bad_words + [b.title() for b in bad_words] # previous bad words + title case
 
-    vocab_bad_words = [word for word in vocab if word in bad_words]
-    bad_words_ids = []
-    for word in vocab_bad_words:
-        tokenized_word = tokenizer([word], add_special_tokens=False).input_ids[0]
-        bad_words_ids.append(tokenized_word)
+    bad_words_ids = tokenizer(bad_words, add_special_tokens=False).input_ids
     print({
-        'bad_words': vocab_bad_words,
+        'bad_words': bad_words,
         'bad_word_ids': bad_words_ids
     })
     return bad_words_ids
